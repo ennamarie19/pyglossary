1caf8a6172a3ed6db9c2a698b3b9f68e7b3df9df
ennamarie19 <dallmeyerm@att.net>
2022-12-31 23:00:02 -0600

    Fixed things

--- mayhem/fuzz_gloss.py
+++ mayhem/fuzz_gloss.py
@@ -3,18 +3,29 @@
 import atheris
 import sys
 import fuzz_helpers
+import io
+from contextlib import contextmanager
 
 with atheris.instrument_imports():
     from pyglossary import Glossary
+@contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = io.StringIO()
+    sys.stderr = io.StringIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
 
 Glossary.init()
 glos = Glossary()
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
-        with fdp.ConsumeTemporaryFile('', all_data=True, as_bytes=True) as f:
-            glos.convert(f, outputFilename="/dev/null", outputFormat="Tabfile")
-    except ValueError:
+        with fdp.ConsumeTemporaryFile('', all_data=True, as_bytes=True) as f, nostdout():
+            glos.convert(inputFilename=f, inputFormat="", outputFilename="/dev/null", outputFormat="")
+    except (UnicodeDecodeError, ValueError, PermissionError):
         return -1
 
 def main():
